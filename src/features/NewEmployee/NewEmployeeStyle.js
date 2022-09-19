50505100a6c6f34bd183a7de979cd383897b0010
Damian Mares <damian.mares@totalsoft.ro>
2022-09-19 17:48:18 +0300

    demianski

--- src/features/NewEmployee/NewEmployeeStyle.js
+++ src/features/NewEmployee/NewEmployeeStyle.js
@@ -1,6 +1,6 @@
 
 
-const Title = () => {
+const Style = () => {
   
     return {
         Title: {
@@ -13,8 +13,53 @@ const Title = () => {
         lineHeight: '64px',
         
       
-        color: '#202020'}
+        color: '#202020'},
+         lable: {
+            marginTop: '25px',
+            width: '334px',
+            height: '22px',
+        
+            fontFamily: 'Open Sans',
+            fontStyle: 'normal',
+            fontWeight: '700',
+            fontSize: '16px',
+            lineHeight: '22px',
+            /* identical to box height */
+        
+            letterpacing: '1px',
+        
+            color: '#C7C7C7'
+          },
+        
+          box: {
+            marginTop: '12px',
+            boxSizing: 'border-box',
+        
+            width: '350px',
+            height: '60px',
+        
+            border: '1px solid #C2C2C2',
+            borderRadius: '14px'
+          },
+
+          date: {
+            width: '140px',
+            height: '22px',
+      
+            fontFamily: 'Open Sans',
+            fontStyle: 'normal',
+            fontWeight: '700',
+            fontSize: '16px',
+            lineHeight: '22px',
+            /* identical to box height */
+      
+            letterSpacing: '1px',
+      
+            color: '#C7C7C7',
+            marginBottom: '15px'
+          },
     }
   }
+ 
   
-  export default Title
+  export default Style
