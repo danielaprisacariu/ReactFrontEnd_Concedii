2b503c9a62251fd12e01f6dc640d2d84536a290f
Damian Mares <damian.mares@totalsoft.ro>
2022-09-19 15:16:41 +0300

    demian

--- src/features/NewEmployee/NewEmployeeCard.js
+++ src/features/NewEmployee/NewEmployeeCard.js
@@ -3,17 +3,5 @@ import { Typography, Grid } from '@material-ui/core'
 import { useToast } from '@bit/totalsoft_oss.react-mui.kit.core'
 import '../../features/NewEmployee/NewEmployeeCSS.css'
 
-function NewEmployee() {
-  const addToast = useToast()
-  addToast('Welcome', 'success')
-  return (<Fragment>
-    <Typography>
-    <h1> Add New Employee </h1>
-    </Typography>
-   </Fragment>
-   
-  )
-}
 
-export default NewEmployee
 
