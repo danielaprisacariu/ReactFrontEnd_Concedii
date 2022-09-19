2f78435d18e2f5ad26867438996b5d94519bbed7
Erwin Brustur <erwin.brustur@totalsoft.ro>
2022-09-19 11:34:11 +0300

    eb

--- src/components/CerereConcediu/CerereConcediuContainer.js
+++ src/components/CerereConcediu/CerereConcediuContainer.js
@@ -1,22 +1,24 @@
-import React, { Fragment} from 'react'
+import React, { Fragment } from 'react'
 import { Typography, Grid } from '@material-ui/core'
 import { useToast } from '@bit/totalsoft_oss.react-mui.kit.core'
-import {DatePickers} from './DatePickerCard'
-
-
+import { DatePickers } from './DatePickerCard'
+import '../../features/allCSS/CerereConcediuContainerCSS.css'
 
 function CerereConcediu() {
-    const addToast = useToast()
-    addToast('Welcome', 'success')
-    return (
-      <Fragment>
-        <Typography></Typography>
+  const addToast = useToast()
+  addToast('Welcome', 'success')
+  return (
+    <Fragment>
+      <Typography></Typography>
+      <h1> Adauga concediu </h1>
+      <div className='DA'>
         <DatePickers> </DatePickers>
+
         <DatePickers> </DatePickers>
-       
-        <Grid></Grid>
-      </Fragment>
-    )
-  }
-  
+      </div>
+      <Grid></Grid>
+    </Fragment>
+  )
+}
+
 export default CerereConcediu
