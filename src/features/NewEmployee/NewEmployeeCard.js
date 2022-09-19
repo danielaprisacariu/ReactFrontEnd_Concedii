9935ec8192898f7df3b07484447ca75bf2ad336a
Damian Mares <damian.mares@totalsoft.ro>
2022-09-19 14:39:09 +0300

    damian

--- src/features/NewEmployee/NewEmployeeCard.js
+++ src/features/NewEmployee/NewEmployeeCard.js
@@ -1,5 +1,19 @@
 import React, { Fragment } from 'react'
 import { Typography, Grid } from '@material-ui/core'
 import { useToast } from '@bit/totalsoft_oss.react-mui.kit.core'
+import '../../features/NewEmployee/NewEmployeeCSS.css'
 
+function NewEmployee() {
+  const addToast = useToast()
+  addToast('Welcome', 'success')
+  return (<Fragment>
+    <Typography>
+    <h1> Add New Employee </h1>
+    </Typography>
+   </Fragment>
+   
+  )
+}
+
+export default NewEmployee
 
--- src/features/NewEmployee/NewEmployeeContainer.js
+++ src/features/NewEmployee/NewEmployeeContainer.js
@@ -2,17 +2,5 @@ import React, { Fragment } from 'react'
 import { Typography, Grid } from '@material-ui/core'
 import { useToast } from '@bit/totalsoft_oss.react-mui.kit.core'
 import '../../features/NewEmployee/NewEmployeeCSS.css'
+import 'NewEmployeeCard'
 
-function NewEmployee() {
-  const addToast = useToast()
-  addToast('Welcome', 'success')
-  return (<Fragment>
-    <Typography>
-    <h1> Add New Employee </h1>
-    </Typography>
-   </Fragment>
-   
-  )
-}
-
-export default NewEmployee
