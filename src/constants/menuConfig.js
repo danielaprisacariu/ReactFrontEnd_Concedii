56245405aa65395d7a7ad78ed731fa32104c85cf
Damian Mares <damian.mares@totalsoft.ro>
2022-09-19 11:35:10 +0300

    demianski

--- src/constants/menuConfig.js
+++ src/constants/menuConfig.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Add, Dashboard } from '@material-ui/icons'
 
-const menuItems = [{ icon: <Dashboard />, text: 'NavBar.Dashboard', path: '/dashboard', name: 'Dashboard' },
-                    { icon: <Add />, text: 'NavBar.NewEmployee', path: '/newEmployee', name: 'NewEmployee' }]
+const menuItems = [{ icon: <Dashboard />, text: 'NavBar.Dashboard', path: '/dashboard', name: 'Dashboard' },]
 
 export default menuItems
