f8625d7093def36da6373ff0319bb56647093433
Damian Mares <damian.mares@totalsoft.ro>
2022-09-19 16:27:22 +0300

    demianski

--- src/features/NewEmployee/NewEmployeeComponent.js
+++ src/features/NewEmployee/NewEmployeeComponent.js
@@ -1,8 +1,22 @@
 import React, { Fragment } from 'react'
 import { Typography, Grid } from '@material-ui/core'
 import { useToast } from '@bit/totalsoft_oss.react-mui.kit.core'
-import '../../features/NewEmployee/NewEmployeeCSS.css'
+import appStyle from './NewEmployeeStyle';
+import { makeStyles } from '@material-ui/core'
 
-
+const useStyles = makeStyles(appStyle)
+function NewEmployeeComponent() {
+    const addToast = useToast()
+    addToast('Welcome', 'success')
+    const classes = useStyles();
+    return (<Fragment>
+      <Typography>
+      <h1 className={classes.Title}> Add New Employee </h1>
+      </Typography>
+     </Fragment>
+     
+    )
+  }
   
+  export default NewEmployeeComponent
 
---
+++ src/features/NewEmployee/NewEmployeeStyle.js
@@ -0,0 +1,20 @@
+
+
+const Title = () => {
+  
+    return {
+        Title: {
+        width: '20',
+        height: '64px',
+        fontFamily: 'Cairo',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize: '34px',
+        lineHeight: '64px',
+        
+      
+        color: '#202020'}
+    }
+  }
+  
+  export default Title
