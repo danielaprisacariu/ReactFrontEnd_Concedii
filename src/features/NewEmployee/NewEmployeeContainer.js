9f5cb29c3837216297125d34e35d7770dfc5a9f6
Erwin Brustur <erwin.brustur@totalsoft.ro>
2022-09-19 16:26:37 +0300

    eb

--- src/features/NewEmployee/NewEmployeeContainer.js
+++ src/features/NewEmployee/NewEmployeeContainer.js
@@ -1,17 +1,17 @@
 import React, { Fragment } from 'react'
 import { Typography, Grid } from '@material-ui/core'
 import { useToast } from '@bit/totalsoft_oss.react-mui.kit.core'
-import '../../features/NewEmployee/NewEmployeeCSS.css'
+import './NewEmployeeCSS.css'
 
 function NewEmployee() {
   const addToast = useToast()
   addToast('Welcome', 'success')
-  return (<Fragment>
-    <Typography>
-    <h1> Add New Employee </h1>
-    </Typography>
-   </Fragment>
-   
+  return (
+    <Fragment>
+      <Typography>
+        <h1> Add New Employee </h1>
+      </Typography>
+    </Fragment>
   )
 }
 
