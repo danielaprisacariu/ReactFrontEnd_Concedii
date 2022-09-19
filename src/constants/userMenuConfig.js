f3e4e866c55f09edf805e6e58832d763eef4e41a
Erwin Brustur <erwin.brustur@totalsoft.ro>
2022-09-19 16:26:26 +0300

    eb

--- src/constants/userMenuConfig.js
+++ src/constants/userMenuConfig.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Person from '@material-ui/icons/Person'
 import { CalendarToday, MenuBook } from '@material-ui/icons'
 import { CalendarViewDayRounded } from '@material-ui/icons'
-import CerereConcediu from '../features/CerereConcediu/CererereConcediuContainer'
+import CerereConcediuContainer from '../features/CerereConcediu/CererereConcediuContainer'
 
 const userMenuItems = [
   { icon: <Person />, text: 'MyProfile', path: '/myProfile', name: 'MyProfile' },
   { icon: <CalendarViewDayRounded />, text: 'Toate Concediile', path: '/toateconcediile', nume: 'Toate Concediile' },
   { icon: <MenuBook />, text: 'HolidaysMenu', path: '/holidaysMenu', name: 'HolidaysMenu' },
-  { icon: <CalendarToday />, text: 'Cerere Concediu', path: '/CerereConcediu', name: 'Cerere Concediu' }
+  { icon: <CalendarToday />, text: 'CerereConcediu', path: '/CerereConcediuContainer', name: 'Cerere Concediu' }
 ]
 
 export default userMenuItems
