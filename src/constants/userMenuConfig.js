c6c4b72beda68f83d26656a5671bb2d489db8afe
Erwin Brustur <erwin.brustur@totalsoft.ro>
2022-09-19 09:49:33 +0300

    eb

--- src/constants/userMenuConfig.js
+++ src/constants/userMenuConfig.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Person from '@material-ui/icons/Person'
 
-const userMenuItems = [{ icon: <Person />, text: 'MyProfile', path: '/myProfile', name: 'MyProfile' }]
+const userMenuItems = [{ icon: <Person />, text: 'MyProfile', path: '/myProfile', name: 'MyProfile' },
+                        { icon: <Person />, text: 'CerereConcediu', path: '/CerereConcediuContainer', name: 'CerereConcediu' }]
 
 export default userMenuItems
