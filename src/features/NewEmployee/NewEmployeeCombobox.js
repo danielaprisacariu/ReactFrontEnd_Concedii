4305729576ee5a9ca24964eca48c2dadb5cccf2b
Damian Mares <damian.mares@totalsoft.ro>
2022-09-22 16:55:33 +0300

    dm

--- src/features/NewEmployee/NewEmployeeCombobox.js
+++ src/features/NewEmployee/NewEmployeeCombobox.js
@@ -1,29 +1,12 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField'
 import Autocomplete from '@material-ui/lab/Autocomplete'
-import { makeStyles } from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
-import Style from './NewEmployeeStyle'
 
 export function NewEmployeeCombobox(props) {
-  const { arrayDataSource } = props
-  const { onChange, propertyName, id } = props
-
-  const useStyles = makeStyles(theme => ({
-    container: {
-      display: 'flex',
-
-      flexWrap: 'wrap'
-    },
-
-    textField: {
-      marginLeft: theme.spacing(1),
-
-      marginRight: theme.spacing(1),
-
-      width: 200
-    }
-  }))
+  const { arrayDataSource, onChange, propertyName, id } = props
+  console.log('id' + id)
+  console.log(arrayDataSource)
 
   return (
     <div>
@@ -32,8 +15,8 @@ export function NewEmployeeCombobox(props) {
         getOptionLabel={element => element.nume}
         value={id}
         style={{ width: 350 }}
-        renderInput={params => <TextField {...params} variant='outlined' />}
         onChange={(event, value) => onChange(propertyName, value.id)}
+        renderInput={params => <TextField {...params} variant='standard' />}
       />
     </div>
   )
