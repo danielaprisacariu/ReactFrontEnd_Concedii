e800d0ac65263f8360ca202d23bf223e12e02ad5
Daniela Prisacariu <daniela.prisacariu@totalsoft.ro>
2022-09-27 12:03:43 +0300

    dp

--- src/features/CerereConcediu/CerereConcediuReducer.js
+++ src/features/CerereConcediu/CerereConcediuReducer.js
@@ -1,13 +1,12 @@
 import Moment from 'moment'
 export const initialState = {
-  id: null,
   tipConcediuId: null,
   dataInceput: Moment().format('YYYY-MM-DD'),
   dataSfarsit: null,
   inlocuitorId: null,
   comentarii: null,
   stareConcediuId: 1,
-  angajatId: null,
+  angajatId: 24,
   zileConcediu: null
 }
 
@@ -19,8 +18,8 @@ export const tipuriConcedii = [
 ]
 
 export const inlocuitori = [
-  { id: 1, nume: 'Popa Dorin' },
-  { id: 2, nume: 'Radu Ionescu' }
+  { id: 23, nume: 'Popa Dorin' },
+  { id: 25, nume: 'Radu Ionescu' }
 ]
 
 export function CerereConcediuReducer(state, action) {
