ae7df12cad9cf6962aaea85b2e1b03601f08e086
Damian Mares <damian.mares@totalsoft.ro>
2022-09-28 09:46:56 +0300

    demian

--- src/features/NewEmployee/NewEmployeeContainer.js
+++ src/features/NewEmployee/NewEmployeeContainer.js
@@ -2,9 +2,25 @@ import React, { useReducer } from 'react'
 import { useHistory } from 'react-router-dom'
 import NewEmployeeComponent from './NewEmployeeComponent'
 import { NewEmployeeReducer, initialState, department, fct } from './NewEmployeeState'
+import { useMutation } from '@apollo/client'
+import { useToast } from '@bit/totalsoft_oss.react-mui.kit.core'
+import { INPUT_NEW_ANGAJAT } from './input'
 
 export default function NewEmployeeContainer() {
+  const addToast = useToast()
   const [state, dispatch] = useReducer(NewEmployeeReducer, initialState)
+  const [updateProcess, { loading: saving, _data, _error }] = useMutation(INPUT_NEW_ANGAJAT, {
+    onCompleted: () => {
+      addToast('Inserare realizata cu succes!', 'success')
+      //history.push({ pathname: `/NewEmployee` })
+    },
+    onError: error => addToast('Error', error)
+  })
+  const handleSave = () => {
+    const newEmployee = state
+    console.log('newEmployee', newEmployee)
+    updateProcess({ variables: { input: newEmployee } })
+  }
   const onPropertyChange = (propertyName, value) => {
     dispatch({ type: 'OnPropertyChange', propertyName, value })
     console.log(state)
@@ -18,6 +34,7 @@ export default function NewEmployeeContainer() {
       fct={fct}
       department={department}
       onChange={onPropertyChange}
+      onHandleSave={handleSave}
       state={state}
       onHistoryClick={handleClick}
     ></NewEmployeeComponent>
